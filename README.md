e83530cc8934c766b245844120af9bc182fe4525
antsfreedom <1639262775@qq.com>
2017-08-13 18:10:39 +0800

    library

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # third-library
 总结了一些常用的库
+
+
+## arguements  ## 
+
+	//arguments 相当用于数组，循环求和,在不知道个数以及数据类型的情况下可用
+	 function sum(){
+	 	var result = 0;
+	 	for(var i=0;i<arguments.length;i++){
+	 		result +=arguments[i];
+	 	}
+	 		return result;
+	 }
+	 alert(sum(2,3,4,3,5,7,4,5))
+
+获取样式
+
+
+	var obox = document.getElementById("box");
+	// alert(getComputedStyle(obox,false).width);//兼容火狐 chorme
+	// alert(obox.currentStyle.width)  //兼容IE
+	// alert(obox.style.width)   //针对行间样式可用此方式直接获取，上面主要针对非行间样式的获取
---
+++ jsLibrary.js
@@ -0,0 +1,113 @@
+//事件捕获
+	function fun(ele,eve,handler,type){
+		if(ele.attachEvent){
+			ele.attachEvent("on"+eve,handler)
+		}else{
+			ele.addEventListener(eve,handler,type)
+		}
+	}
+
+
+//onclick
+	document.onclik = function(e){	
+		var oEvent = e || event;
+		alert('x:'+oEvent.clientX + 'y:'+oEvent.clientY)		
+	}
+
+
+//keydown
+	document.onkeydown = function(eve){	
+	if(eve.keyCode){
+		alert(eve.keyCode)
+	}else{
+		alert(eve.which)
+		}
+	}
+
+//阻止冒泡
+	var e = e || window.event;
+	  if ( e && e.stopPropagation ){
+	      e.stopPropagation();
+	  }else{
+	      e.cancelBubble = true;
+	  }
+
+//阻止默认事件
+	var olink = document.getElementById("link")
+	olink.onclick= function(e){
+		var e = e||window.event;
+		if(e && e.preventDefault){
+			e.preventDefault();
+		}else{
+			e.returnValue = false;
+		}
+	}
+
+//事件委托
+	oUl.onmouseover = function(e){  
+	    //为了兼容IE
+	var oEvent = e||window.event; 
+	var target = oEvent.target|| oEvent.srcElement;  
+	
+	if(target.nodeName.toLowerCase() == 'li'){ 
+		target.style.background = 'red';  
+		} 	 
+	}
+
+//获取元素的样式
+	function getStyle (obj,style){
+		if(obj.currentStyle){
+			return obj.currentStyle[style]
+		}else{
+			return getComputedStyle(obj,false)[style];
+		}		
+	}
+
+	//封装 设置cookie
+	function setcookie(name,value,iday){
+		var oDate = new Date();
+		oDate.setDate(oDate.getDate()+iday);
+			document.cookie = name+ '='+value+'; expires='+oDate.toUTCString();
+	}
+//封装 删除cookie	
+	function removecookie(name){
+		setcookie(name,1,-1)
+	}
+
+	//封装获取cookie
+	function getcookie(name){
+		var arr =document.cookie.split('; ');
+		for(var i=0;i<arr.length;i++){
+			var arr2 = arr[i].split('=');
+			if(arr2[0]==name){
+				alert(arr2[1])
+			}
+		}
+		return'false';
+	}
+
+//用jquery封装的ajax
+
+	function fun(option){
+	var type = option.type;
+	var url = option.url;
+	var judg = option.judg;
+	var success =option.success;
+
+	var xhr = null;
+	if(window.XMLHttpRequest){
+	  xhr = new XMLHttpRequest();
+	}else{
+		xhr = new ActiveXObject("Microsoft.XMLHttp")
+	}
+	xhr.open(type,url,judg);
+	xhr.send();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState ==4){
+			if(xhr.status ==200){
+				success && success(JSON.parse(xhr.responseText));		
+			}
+		}
+	}
+
+}
